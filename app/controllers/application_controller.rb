536ecef0007464404c8df3aaa988d9c7a9389630
daliayan <daliayan5@gmail.com>
2021-04-09 20:06:33 -0400

    build a Sinatra application that will allow users to sign up for, log into, and then log out of your application.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,10 +18,9 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
-    @user.save
-    session[:user_id] = @user.id
-
-    redirect '/users/home'
+    @user.save 
+    session[:user_id] = @user.id 
+    redirect '/users/home' 
   end
 
   get '/sessions/login' do
@@ -45,8 +44,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/users/home' do
-
     @user = User.find(session[:user_id])
     erb :'/users/home'
-  end
+  end 
 end
